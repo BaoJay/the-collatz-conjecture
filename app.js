1fceba611ee46d20476eb3c6132a5d9f0d71ae0a
Gia Bao <giabao.nguyenhoang1001@gmail.com>
2022-06-06 11:29:49 +0700

    create the collatz function

---
+++ app.js
@@ -0,0 +1,11 @@
+function collatz(n) {
+  if (n == 1) {
+    return 0;
+  } else if (n % 2 == 0) {
+    return 1 + collatz(n / 2);
+  } else {
+    return 1 + collatz(3 * n + 1);
+  }
+}
+
+// let numBao = null;
